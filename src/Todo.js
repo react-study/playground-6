b8b4f33e6f574dccb32af80c4504b1b4a9e23b37
hyunji-bang <groovehyunji@gmail.com>
2017-07-22 14:31:57 +0900

    onDoubleClick event

--- src/Todo.js
+++ src/Todo.js
@@ -1,26 +1,40 @@
 import React from 'react';
 
-const Todo = ({
-    text,
-    isDone,
-    isEditing,
-    deleteTodo,
-    startEdit
-}) => (
-    <li className={`todo-item${isEditing ? ' editing' : ''}`}>
-        <button className="toggle" />
-        <div className="todo-item__view">
-            <div 
-                className="todo-item__view__text"
-                onDoubleClick={startEdit}>
-                {text}
-            </div>
-            <button 
-                className="todo-item__destroy"
-                onClick={deleteTodo}/>
-        </div>
-        <input type="text" className="todo-item__edit"/>
-    </li>
-);
-
+class Todo extends React.Component {
+    // componentWillUpdate는 nextProps, nextState 를 인자로 받음
+    componentDidUpdate(prevProps) { // DidUpdate는 렌더링이 이미 한 시점이라서, 바뀌기전 props, 현재의 props 다 받을 수 있음.
+        if(this.props.isEditing && !prevProps.isEditing) {
+            this._inputDom.focus();
+            this._inputDom.value = this.props.text;
+        }
+    }
+    render() {
+        const {
+            text,
+            isDone,
+            isEditing,
+            deleteTodo,
+            startEdit
+        } = this.props;
+        return (
+            <li className={`todo-item${isEditing ? ' editing' : ''}`}>
+                <button className="toggle" />
+                <div className="todo-item__view">
+                    <div 
+                        className="todo-item__view__text"
+                        onDoubleClick={startEdit}>
+                        {text}
+                    </div>
+                    <button 
+                        className="todo-item__destroy"
+                        onClick={deleteTodo}/>
+                </div>
+                <input type="text" 
+                        className="todo-item__edit"
+                        ref={ref =>{
+                            this._inputDom = ref}}/>
+            </li>
+        )
+    }
+}
 export default Todo;
