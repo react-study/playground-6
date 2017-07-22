064fc51c3e567dbe89ecb5ce1a5d39eb86625385
hyunji-bang <groovehyunji@gmail.com>
2017-07-22 16:18:12 +0900

    stateless component REFACTORING in Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-class Footer extends React.Component{
-    render(){
-        return (
-            <footer className="footer">
-                <span className="todo-count">
-                    0 items left
-                </span>
-                <ul className="todo-filters">
-                    <li><a href="">All</a></li>
-                    <li><a href="">Active</a></li>
-                    <li><a href="">Completed</a></li>
-                </ul>
-                <button 
-                    className="todo-delete-completed"
-                    onClick={this.props.clearCompleted}>
-                        Clear Completed
-                </button>
-            </footer>
-        )
-    }
-}
+const Footer = ({
+    clearCompleted
+}) => (
+    <footer className="footer">
+        <span className="todo-count">
+            0 items left
+        </span>
+        <ul className="todo-filters">
+            <li><a href="">All</a></li>
+            <li><a href="">Active</a></li>
+            <li><a href="">Completed</a></li>
+        </ul>
+        <button 
+            className="todo-delete-completed"
+            onClick={clearCompleted}>
+                Clear Completed
+        </button>
+    </footer>
+)
 
 export default Footer;
