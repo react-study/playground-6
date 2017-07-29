fdffffbedc30ac24b6f3fbfbdf78a35a51d5e3cf
MM\Julie <laststardust999@gmail.com>
2017-07-29 17:05:24 +0900

    TodoApp4 router

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ class App extends React.Component {
     super();
     this.state = {
       todos: [],
-      editingId: null,
-      filter: 'All'
+      editingId: null
     };
   }
 
@@ -125,27 +124,29 @@ class App extends React.Component {
     });
  }
 
- selectFilter = filter => {
-   this.setState({
-     filter
-   })
- }
-
   render() {
     const {
       todos,
-      editingId,
-      filter
+      editingId
     } = this.state;
+
+    const {
+      match:{
+        params
+      }
+    } = this.props;
+
+    const filter = params.filter || 'All';
+    // this.props.match.params === {filter: 'active'}
 
     const activeLength = todos.filter(v => !v.isDone).length;
     const completedLength = todos.length - activeLength;
 
     let filteredTodos = null;
     switch(filter) {
-      case 'Active' : filteredTodos = todos.filter(v => !v.isDone); break;
-      case 'Completed' : filteredTodos = todos.filter(v => v.isDone); break;
-      case 'All':
+      case 'active' : filteredTodos = todos.filter(v => !v.isDone); break;
+      case 'completed' : filteredTodos = todos.filter(v => v.isDone); break;
+      case 'all':
       default: filteredTodos = todos;
     }
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
 import App from './App'
 ReactDOM.render(
-    (<App />),
-    document.getElementById('root')
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/:filter" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
 );
