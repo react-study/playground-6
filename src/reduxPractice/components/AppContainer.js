6981d30e7c8a689fabfc4df7f311bd0abec780f8
hyunji-bang <groovehyunji@gmail.com>
2017-08-05 16:06:05 +0900

    mapStateToProps, mapDispatchToProps init

--- src/reduxPractice/components/AppContainer.js
+++ src/reduxPractice/components/AppContainer.js
@@ -4,26 +4,27 @@ import AccountList from './AccountList';
 
 const mapStateToProps = state => ({
     // state가 props로 변환된 데이터들
+    accountData:  ???
 })
 const mapDispatchToProps = dispatch => ({
     // 메소드(action을 dispatch하는 메소드)들
+    calculate: (type, money) => dispatch({ //dispatch는 액션객체을 날린다.
+
+    })
 })
 
 class AppContainer extends React.Component {
-    state = {
-        accountData: []
-    };
 
     // 2번째 방법 ---------------------------------------------------
     calculate = (type, money) => {
         money = money * 1; //숫자형으로 바꿔주기;
-        const prevAccount = this.state.accountData;
+        const prevAccount = this.props.accountData;
         const prevLength = prevAccount.length;
         const lastResult = prevLength? (prevAccount[prevLength - 1].result) : 0;   // lastResult state로 따로 빼도 됨.
 
         this.setState({
             accountData: [
-                ...this.state.acccountData, {
+                ...this.props.acccountData, {
                     type,
                     money,
                     result: lastResult + (type === 'deposit' ? 1 : -1 ) * money
@@ -35,8 +36,8 @@ class AppContainer extends React.Component {
     render() {
         return (
             <div>
-                <InputBox calculate={this.calculate}/>
-                <AccountList accountData={this.state.accountData}/>
+                <InputBox calculate={this.props.calculate}/>
+                <AccountList accountData={this.props.accountData}/>
             </div>
         );
     }
