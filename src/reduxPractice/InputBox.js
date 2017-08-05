eb5d649d173ae7981511d5b870fbed8c55293500
hyunji-bang <groovehyunji@gmail.com>
2017-08-05 14:32:26 +0900

    InputBox

---
+++ src/reduxPractice/InputBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class InputBox extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="text"/>
+                <button onClick={}>입금</button>
+                <button onClick={}>출금</button>
+            </div>
+        )
+    }
+}
+
+export default InputBox;
