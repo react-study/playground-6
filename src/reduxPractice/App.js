acb342bd06f89907f6bec469a191fd630106ba94
hyunji-bang <groovehyunji@gmail.com>
2017-08-05 14:44:09 +0900

    making Methods in App.js (2st method)

--- src/reduxPractice/App.js
+++ src/reduxPractice/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
         calculate(type, money) 안에서 type을 분기하여 처리하는 방법이 있다.
     */
 
-    // 1번째 방법 ---------------------------------------------------
+    /* 1번째 방법 ---------------------------------------------------
     deposit(money) {
         money = money * 1; //숫자형으로 바꿔주기;
         // money = +money  이렇게 암묵적 형변환도 가능
@@ -20,7 +20,7 @@ class App extends React.Component {
 
         const prevAccount = this.state.accountList;
         const prevLength = prevAccount.length;
-        const lastResult = prevLength? (prevAccount[prevLength - 1].result) : 0;
+        const lastResult = prevLength? (prevAccount[prevLength - 1].result) : 0;   // lastResult state로 따로 빼도 됨.
 
         this.setState({
             accountData: [
@@ -32,11 +32,31 @@ class App extends React.Component {
             ]
         })
     }
+    */
+
+
+    // 2번째 방법 ---------------------------------------------------
+    calculate = (type, money) => {
+        money = money * 1; //숫자형으로 바꿔주기;
+        const prevAccount = this.state.accountList;
+        const prevLength = prevAccount.length;
+        const lastResult = prevLength? (prevAccount[prevLength - 1].result) : 0;   // lastResult state로 따로 빼도 됨.
+
+        this.setState({
+            accountData: [
+                ...this.state.acccountData, {
+                    type,
+                    money,
+                    result: lastResult + (type === 'deposit' ? 1 : -1 ) * money
+                }
+            ]
+        })
+    }
 
     render() {
         return (
             <div>
-                <Input/>
+                <Input calculate={this.calculate}/>
                 <AccountList accountData={this.state.accountData}/>
             </div>
         );
