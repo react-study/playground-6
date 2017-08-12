a0406a0b8994819fbf164ed61d13ac2b9d0811f3
gomugom <power4ce@gmail.com>
2017-08-12 16:03:24 +0900

    리덕스 썽크

--- src/reduxPractice/store.js
+++ src/reduxPractice/store.js
@@ -1,15 +1,26 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import bankReducer from './reducers/bankReducer';
 import tabReducer from './reducers/tabReducer';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(
-    combineReducers({
-        bank: bankReducer,
-        tab: tabReducer
-    }),
+const logger = createLogger({
+    collapsed: true,
+    diff: true,
+    timestamp: true
+});
+
+const reducers = combineReducers({
+    bank: bankReducer,
+    tab: tabReducer
+});
+
+const others = compose(
+    applyMiddleware(thunk, logger),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+const store = createStore(reducers, others);
 export default store;
 
 
