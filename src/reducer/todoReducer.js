dd7700e337807d372d35ce93ccefb415f6f7efd9
hyunji-bang <groovehyunji@gmail.com>
2017-08-19 15:49:42 +0900

    todoReducer

--- src/reducer/todoReducer.js
+++ src/reducer/todoReducer.js
@@ -7,19 +7,19 @@ const initialState = {
 
 const TodoReducer = (state = initialState, action) => {
     switch(action.type) {
-        case 'GET_TODOS_SUCCESS':
+        case 'GET_TODOS':
             return update(state, {
                 todos: {
                     $set: action.todos
                 }
             });
-        case 'ADD_TODO_SUCCESS':
+        case 'ADD_TODO':
             return update(state, {
                 todos: {
                     $push: [ action.newTodo ]
                 }
             });
-        case 'DELETE_TODO_SUCCESS':
+        case 'DELETE_TODO':
             return update(state, {
                 todos: {
                     $splice: [
@@ -27,10 +27,52 @@ const TodoReducer = (state = initialState, action) => {
                     ]
                 }
             });
-        case 'EDIT_TODO':
+
+        case 'START_EDIT':
             return update(state, {
                 editingId: { $set: action.id }
             });
+
+        case 'SAVE_TODO':
+            return update(state, {
+                todos: {
+                    [state.todos.findIndex(v => v.id === action.id)]: {
+                        $set: action.editedTodo
+                    }
+                },
+                editingId: {
+                    $set: null
+                }
+            })
+
+        case 'CANCEL_EDIT':
+            return update(state, {
+                editingId: {
+                    $set: null
+                }
+            })
+
+        case 'TOGGLE_TODO':
+            return update(state, {
+                todos: {
+                    [state.todos.findIndex(v => v.id === action.id)]: {
+                        $set: action.editedTodo
+                    }
+                }
+            })
+
+        case 'TOGGLE_ALL':
+            return update(state, {
+                todos: {
+                    $set: action.editedTodos
+                }
+            })
+        case 'CLEAR_COMPLETED':
+            return update(state, {
+                todos: {
+                    $apply: todos => todos.filter(v => !v.isDone)
+                }
+            })
         default: return state;
     }
 }
