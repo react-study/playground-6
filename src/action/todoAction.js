790fb75a4d4b6c849a97ed8bbd992c4ecead2d3b
gomugom <power4ce@gmail.com>
2017-08-19 16:11:55 +0900

    리

--- src/action/todoAction.js
+++ src/action/todoAction.js
@@ -1,3 +1,4 @@
+import constants from '../constants';
 import axios from 'axios';
 const ax = axios.create({
     baseURL: 'http://localhost:2403/todos',
@@ -6,32 +7,86 @@ const ax = axios.create({
 
 const todoAction = {
     getTodos: () => dispatch => {
-        dispatch({
-            type: 'GET_TODOS_REQUEST'
-        });
-        
         ax.get('/')
-        .then(res => {
-            dispatch({
-                type: 'GET_TODOS_SUCCESS',
-                todos: res.data
+            .then(res => {
+                dispatch({
+                    type: constants.todo.get,
+                    todos: res.data
+                });
             });
-        })
-        .catch(err => {
+    },
+    addTodo: text => dispatch => {
+        ax.post('/', { text })
+            .then(res => {
+                dispatch({
+                    type: constants.todo.add,
+                    newTodo: res.data
+                })
+            });
+    },
+    deleteTodo: id => dispatch => {
+        ax.delete(`/${id}`)
+            .then(() => {
+                dispatch({
+                    type: constants.todo.delte,
+                    id
+                });
+            });
+    },
+    startEdit: id => ({
+        type: constants.todo.startEdit,
+        id
+    }),
+    saveTodo: (id, newText) => dispatch => {
+        ax.put(`/${id}`, { text: newText })
+            .then(res => {
+                dispatch({
+                    type: constants.todo.save,
+                    id,
+                    editedTodo: res.data
+                });
+            });
+    },
+    cancelEdit: () => ({
+        type: constants.todo.cancelEdit
+    }),
+    toggleTodo: id => (dispatch, getState) => {
+        const newDone = !getState().todos.find(v => v.id === id).isDone;
+        ax.put(`/${id}`, { isDone: newDone })
+            .then(res => {
+                dispatch({
+                    type: constants.todo.toggle,
+                    id,
+                    editedTodo: res.data
+                });
+            });
+    },
+    toggleAll: () => (dispatch, getState) => {
+        const prevTodos = getState().todos;
+        const newDone = !prevTodos.every(v => v.isDone);
+        const axArray = prevTodos.map(v =>
+            ax.put(`/${v.id}`, { isDone: newDone })
+        );
+        axios.all(axArray)
+            .then(res => {
+                dispatch({
+                    type: constants.todo.toggleAll,
+                    editedTodos: res.map(v => v.data)
+                });
+            });
+    },
+    clearCompleted: () => (dispatch, getState) => {
+        const prevTodos = getState().todos;
+        const axArray = prevTodos
+            .filter(v => v.isDone)
+            .map(v => ax.delete(`/${v.id}`));
+        axios.all(axArray)
+        .then(() => {
             dispatch({
-                type: 'GET_TODOS_FAILED',
-                err
+                type: constants.todo.clear
             });
         });
     },
-    addTodo: text => {},
-    deleteTodo: id => {},
-    startEdit: id => {},
-    saveTodo: (id, newText) => {},
-    cancelEdit: () => {},
-    toggleTodo: id => {},
-    toggleAll: () => {},
-    clearCompleted: () => {},
-}
+};
 
 export default todoAction;
--- src/reducer/todoReducer.js
+++ src/reducer/todoReducer.js
@@ -1,5 +1,5 @@
 import update from 'immutability-helper';
-
+import constants from '../constants';
 const initialState = {
     todos: [],
     editingId: null
@@ -7,19 +7,19 @@ const initialState = {
 
 const TodoReducer = (state = initialState, action) => {
     switch(action.type) {
-        case 'GET_TODOS_SUCCESS':
+        case constants.todo.get:
             return update(state, {
                 todos: {
                     $set: action.todos
                 }
             });
-        case 'ADD_TODO_SUCCESS':
+        case constants.todo.add:
             return update(state, {
                 todos: {
                     $push: [ action.newTodo ]
                 }
             });
-        case 'DELETE_TODO_SUCCESS':
+        case constants.todo.delete:
             return update(state, {
                 todos: {
                     $splice: [
@@ -27,9 +27,46 @@ const TodoReducer = (state = initialState, action) => {
                     ]
                 }
             });
-        case 'EDIT_TODO':
+        case constants.todo.startEdit:
             return update(state, {
                 editingId: { $set: action.id }
+            });
+        case constants.todo.save:
+            return update(state, {
+                todos: {
+                    [state.todos.findIndex(v => v.id === action.id)]: {
+                        $set: action.editedTodo
+                    }
+                },
+                editingId: {
+                    $set: null
+                }
+            });
+        case constants.todo.cancelEdit:
+            return update(state, {
+                editingId: {
+                    $set: null
+                }
+            });
+        case constants.todo.toggle:
+            return update(state, {
+                todos: {
+                    [state.todos.findIndex(v => v.id === action.id)]: {
+                        $set: action.editedTodo
+                    }
+                }
+            });
+        case constants.todo.toggleAll:
+            return update(state, {
+                todos: {
+                    $set: action.editedTodos
+                }
+            });
+        case constants.todo.clear:
+            return update(state, {
+                todos: {
+                    $apply: todos => todos.filter(v => !v.isDone)
+                }
             });
         default: return state;
     }
